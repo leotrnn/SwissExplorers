17915bfa515eff657466d9a3d39312c8ba96e88a
leo-trn <leo2005triano@gmail.com>
2024-10-12 20:40:02 +0200

    inversé les liens

--- header.php
+++ header.php
@@ -1,12 +1,11 @@
-
 <div class="header">
+        <a href="index.php">
+                <h1>Swiss Explorers</h1>
+        </a>
         <div class="divHeader">
                 <a href="addLieu.php">Ajouter un marker</a>
                 <a href="about.php">À propos</a>
         </div>
-        <a href="index.php">
-                <h1>Swiss Explorers</h1>
-        </a>
 </div>
 
 
@@ -14,13 +13,13 @@
 <html lang="fr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Carte interactive des lieux visités</title>
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
-    <link rel="stylesheet" href="css/index.css">
-    <link rel="stylesheet" href="css/about.css">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Carte interactive des lieux visités</title>
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+        <link rel="stylesheet" href="css/index.css">
+        <link rel="stylesheet" href="css/about.css">
 </head>
+
 <body>
-        
